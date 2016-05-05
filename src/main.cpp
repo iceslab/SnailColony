fc061660143eb60949e603c6da6bbaa86c5c7444
Bartosz Ciesla <bartociesla@gmail.com>
2016-05-05 13:46:49 +0200

    Dzialajace okno

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <pthread.h>
 #include <ncurses.h>
 #include <iostream>
+#include <unistd.h>
 
 using namespace std;
 
@@ -11,16 +12,27 @@ int main(int argc, char** argv)
 	{
 		WINDOW * win = NULL;
 		cbreak();
+		refresh();
+		int height = 10;
+		int width = 10;
 		int lines, columns;
 		getmaxyx(stdscr, lines, columns);
-		win = newwin(10, 10, 10, 10);
-		box(win, '-' , '|');
-//		wborder(win, '|', '|', '-', '-', '+', '+', '+', '+');
-		wrefresh(win);
-		mvprintw(10, 10, "%d", win);
+		height = lines;
+		width = columns;
+
+		int starty = (lines - height) / 2;     /* Obliczanie środkowej pozycji */
+		int startx = (columns - width) / 2;
+
+//		win = newwin(lines, width, starty, startx);
+//		box(win, '-' , '|');
+//		wprintw(win, "Hello World");
+		wborder(stdscr, '|', '|', '-', '-', '+', '+', '+', '+');
+		wrefresh(stdscr);
+//		mvprintw(10, 10, "%d", win);
 
 		refresh();
-		getchar();
+		sleep(2);
+//		getchar();
 	}
 	endwin();
 	return 0;
